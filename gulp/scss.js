0669fdef1dec0c66c577b167df79a4eea1ad8906
Amy Kapernick <amy@kapers.dev>
2022-04-25 13:58:00 +0800

    [complete] fixing build

--- gulp/scss.js
+++ gulp/scss.js
@@ -5,18 +5,24 @@ const sass = require('gulp-sass')(require('sass'))
 const postcss = require('gulp-postcss')
 const gulpIf = require('gulp-if')
 const header = require('gulp-header')
-const replace = require('gulp-replace')
+const map = require('vinyl-map')
 
 const {compileFiles, cssFiles, browserSync} = require('./config')
 
 const checkVariables = ({path, contents}) => {
 	if (
-		path.match(/variables\.scss/)
+		path.match(/variables\.scss/) ||
+		path.match(/variables(\/|\\)/) ||
+		path.match(/mixins(\/|\\)/) ||
+		path.match(/mixins\.scss/) ||
+		path.match(/animations(\/|\\)/)
 	) {
 		return false
 	}
 
-	console.log({path})
+	if(contents.toString().match(/@use 'variables'/)) {
+		return false
+	}
 
 	return true
 }
@@ -24,7 +30,9 @@ const checkMixins = ({path}) => {
 	if(
 		path.match(/mixins(\/|\\)/) ||
 		path.match(/mixins\.scss/) ||
-		path.match(/animations(\/|\\)/)
+		path.match(/animations(\/|\\)/) ||
+		path.match(/variables\.scss/) ||
+		path.match(/variables(\/|\\)/)
 	) {
 		return false
 	}
@@ -51,22 +59,30 @@ const compileSass = () => (
 				header(`@use 'mixins';\n`)
 			)
 		)
-		
 		// .pipe(
-		// 	replace(
-		// 		/@use 'variables' as var;\\n@use 'variables' as var;\\n/g, 
-		// 		`@use 'variables' as var;\n`
+		// 	gulpIf(
+		// 		({path}) => (path.match('typography.scss')),
+		// 		map((code, filename) => {
+		// 			console.log({
+		// 				filename,
+		// 				string: code.toString()
+		// 			})
+
+		// 			return code
+		// 		})
 		// 	)
 		// )
 		.pipe(
 			postcss()
-			)
+		)
 		.pipe(
 			sass({
 				outputStyle: process.env.NODE_ENV !== 'production' ? 'expanded' : 'compressed',
 				includePaths: [
 					'src/scss',
-				]
+				],
+				quietDeps: true,
+				verbose: true
 			}).on('error', sass.logError)
 		)
 		.pipe(
